99182c1b1de17358a87d1d2b15eb692df6be3ed6
unknown <unknown@example.com>
2019-06-15 19:12:08 +0800

    Initial Commit

---
+++ pages/contact/contact.js
@@ -0,0 +1,83 @@
+// pages/contact/contact.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    person:[
+      {
+        job:"秘书",
+        touch:"李密",
+        phone:"1231231244"
+      },
+      {
+        job: "秘书",
+        touch: "李密",
+        phone: "1231231244"
+      },
+      {
+        job: "秘书",
+        touch: "李密",
+        phone: "123121244"
+      }
+    ]
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
